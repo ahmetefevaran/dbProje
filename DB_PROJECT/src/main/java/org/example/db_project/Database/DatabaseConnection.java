b1a40eeba4f902ddbfbe942f477a6921c2fac962
EnesTiske <109392025+EnesTiske@users.noreply.github.com>
2024-12-14 02:36:58 +0300

    edit dp details

--- DB_PROJECT/src/main/java/org/example/db_project/Database/DatabaseConnection.java
+++ DB_PROJECT/src/main/java/org/example/db_project/Database/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/hastane_uyg";
-    private static final String USER = "proje_user";
-    private static final String PASSWORD = "proje_123";
+    private static final String URL = "jdbc:postgresql://localhost:5432/proje";
+    private static final String USER = "1";
+    private static final String PASSWORD = "1";
 
     public static Connection getConnection() throws SQLException {
         try {
